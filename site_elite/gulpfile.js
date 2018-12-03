530240936b19060c9320a142a8ca1e9732e4d381
Carla <carla.annunciato@eduzz.com>
2018-12-03 13:00:45 -0200

    Initial Commit

---
+++ site_elite/gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    header  = require('gulp-header'),
+    rename = require('gulp-rename'),
+    cssnano = require('gulp-cssnano'),
+    package = require('./package.json');
+    plumber = require('gulp-plumber');
+    concat = require('gulp-concat');
+
+
+var banner = [
+  '/*!\n' +
+  ' * <%= package.name %>\n' +
+  ' * <%= package.title %>\n' +
+  ' * <%= package.url %>\n' +
+  ' * @author <%= package.author %>\n' +
+  ' * @version <%= package.version %>\n' +
+  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
+  ' */',
+  '\n'
+].join('');
+
+gulp.task('css', function () {
+    return gulp.src('src/scss/style.scss')
+    .pipe(plumber())
+    .pipe(sass({
+        errLogToConsole: true,
+        includePaths: [ 'src/styles/scss/' ],
+    }))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('js',function(){
+  gulp.src('src/js/**/*.js')
+    .pipe(plumber())
+    .pipe(concat('scripts.js'))
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(uglify())
+    .pipe(header(banner, { package : package }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(browserSync.reload({stream:true, once: true}));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init(null, {
+        server: {
+            baseDir: "app"
+        }
+    });
+});
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['css', 'js', 'browser-sync'], function () {
+    gulp.watch("src/scss/**", ['css']);
+    gulp.watch("src/js/*.js", ['js']);
+    gulp.watch("app/*.html", ['bs-reload']);
+});
